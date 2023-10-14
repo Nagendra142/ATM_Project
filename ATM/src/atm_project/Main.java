c84892228b76b5b235c71f0e3998350b6067205e
Nagendra Babu <85917805+Nagendra142@users.noreply.github.com>
2023-10-14 23:46:28 +0530

    Add files via upload

---
+++ ATM/src/atm_project/Main.java
@@ -0,0 +1,10 @@
+package atm_project;
+
+import java.sql.SQLException;
+
+public class Main {
+	public static void main(String[] args) throws InterruptedException, SQLException {
+		Login login = new Login();
+		login.loginView();
+	}
+}
